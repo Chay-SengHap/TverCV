40ba2d8e148b68aca263b4ab91c90534c460beb3
Chay-SengHap <chaysenghabb@gmail.com>
2026-06-10 10:34:10 +0700

    set up package

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from 'cors'
+import "dotenv/config"
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/' , (req , res)=> res.send('Server is running'))
+
+app.listen(PORT , ()=>{
+    console.log('Server Running on PORT ' + PORT)
+})
+
